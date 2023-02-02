a3936f45171c341cfae41b9e75404d0a0af8fe52
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-02 18:06:19 +0100

    FEAT super duper basic, style in html...

--- bonus/bonus js/bonus.js
+++ bonus/bonus js/bonus.js
@@ -9,14 +9,17 @@ for (let i = 1; i <= 100; i++) {
 
   if (i % 15 === 0) {
     console.log(i, "fizzbuzz")
+    document.getElementById("output3").innerHTML += "fizzbuzz "
   }
 
   else if (i % 3 === 0) {
     console.log(i, "fizz")
+    document.getElementById("output").innerHTML += "fizz "
   }
 
   else if (i % 5 === 0) {
     console.log(i, "buzz")
+    document.getElementById("output2").innerHTML += "buzz "
   }
 
   else if(i) {
@@ -26,15 +29,16 @@ for (let i = 1; i <= 100; i++) {
 
   // come ultimo altrimenti se, gli dico semplicemente di stampare il numero (i) che in questo caso non deve avere un modulo
   // per trovare un multiplo
-  let multiploDiTre = (i % 3 === 0)
-  console.dir(typeof i)
+  // let multiploDiTre = (i % 3 === 0)
+  // console.dir(multiploDiTre)
 
-  let multiploDiCinque = (i % 5 === 0)
+  // let multiploDiCinque = (i % 5 === 0)
 
-  let multiploDiQuindici = (i % 15 === 0)
+  // let multiploDiQuindici = (i % 15 === 0)
 
 
-  document.getElementById("output").innerHTML = multiploDiTre
+  // document.getElementById("output").innerHTML = multiploDiTre
+  
   
   
 }
