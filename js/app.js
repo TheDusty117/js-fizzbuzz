b8ad5aed1beb2273125e82083acc456855329dd8
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-02 15:12:04 +0100

    FEAT try to understand how it works (?)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("funzionooo!")
+
+
+for (let i = 0; i <= 100; i++) {
+
+  const multiplo= i % 3
+
+  const multiploCinque= i % 5
+
+  const multiploQuindici= i % 15
+
+  console.log(i, multiploTre, multiploCinque, multiploQuindici)
+
+
+}
