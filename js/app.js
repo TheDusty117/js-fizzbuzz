d7ca00cd410a39ed06a1c2b1e06f36e2c8bd806d
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-02 15:51:48 +0100

    FEAT second try (fail)

--- js/app.js
+++ js/app.js
@@ -3,19 +3,26 @@ console.log("funzionooo!")
 
 for (let i = 0; i <= 100; i++) {
 
-  if (i % 3 === 0) {
-    console.log(i, "fizz")
-  }
-
-  if (i % 5 === 0) {
-    console.log(i, "buzz")
-  }
-
-  if (i % 15 === 0) {
-    console.log(i, "fizzbuzz")
-  }
-
   console.log(i)
+
+  if (i % 3 === 0) {
+  console.log("fizz") }
+
+  // if (i % 3 === 0) {
+  //   console.log(i, "fizz")
+  // }
+
+  // if (i % 5 === 0) {
+  //   console.log(i, "buzz")
+  // }
+
+  // if (i % 15 === 0) {
+  //   console.log(i, "fizzbuzz")
+  // }
+
+  // console.log(i)
   
 
 }
+
+//devi dire tutti i numeri da 1 a 100, poi ogni multiplo di 3 "fizz" ogni multiplo di 5 "buzz" ogni multiplo di 15 "fizzbuzz"
