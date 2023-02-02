fd67aaa679fca05dd2c052657300266e047725bf
TheDusty117 <gabrieledipassio@gmail.com>
2023-02-02 15:33:39 +0100

    FEAT fizz working(???)

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 console.log("funzionooo!")
 
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
-  const multiplo= i % 3
-
-  const multiploCinque= i % 5
-
-  const multiploQuindici= i % 15
-
-  console.log(i, multiploTre, multiploCinque, multiploQuindici)
-
+  if (i % 3 === 1) {
+    console.log("fizz")
+  }
+  
+  console.log(i)
 
 }
